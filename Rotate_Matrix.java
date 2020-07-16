3f96cde64c35f26059f439040439a735d4382e0b
Avaneeshakrishna <36323856+Avaneeshakrishna@users.noreply.github.com>
2020-07-16 19:10:15 +0530

    Update Rotate_Matrix.java

--- Rotate_Matrix.java
+++ Rotate_Matrix.java
@@ -10,20 +10,20 @@
 
 consider matrix size = 4			Result                   
            
-			       1 2 3 4      	4 9 5 1                   * * * * 
-			       5 6 7 8	 --> 	5 1 6 2					  * * * *
-			  	   9 1 2 3			6 2 7 3					  * * * *
-			       4 5 6 7			7 3 8 4					  * * * *	
+			       1 2 3 4      	4 9 5 1                   
+			       5 6 7 8	 --> 	5 1 6 2					  
+		               9 1 2 3		6 2 7 3			  
+		       	       4 5 6 7		7 3 8 4				 	
            
  Solution:
             * implement by using layers
             * perform circular rotation on each layer, moving 
               
               
-              left edge to 		--> 	top edge     OR     top[i] 	     = 	left[i]
+              left edge to 	--> 	top edge     OR     top[i] 	 = 	left[i]
               bottom edge to 	--> 	left edge,   OR     left[i] 	 = 	bottom[i]
               right edge to 	--> 	bottom edge, OR     bottom[i]    = 	right[i]
-              top edge to 		-->     right edge	 OR	    right[i] 	 = 	top[i]
+              top edge to 	-->     right edge   OR	    right[i] 	 = 	top[i]
  
 
 
@@ -40,29 +40,29 @@ public class Rotate_Matrix {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the size of the square matrix");
-		int n = sc.nextInt();											// enter size of the matrix
+		int n = sc.nextInt();							     // enter size of the matrix
 		int[][] matrix = new int[n][n];
 		System.out.println("Enter the values");
-		for (int i=0; i<n; i++) {										// enter values for the matrix
+		for (int i=0; i<n; i++) {						     // enter values for the matrix
 			for (int j=0; j<n; j++) {
 				matrix[i][j] = sc.nextInt();
 			}
 		}
-		for (int layer=0; layer<n/2; ++layer) {							// for every layer perform operation
+		for (int layer=0; layer<n/2; ++layer) {					     // for every layer perform operation
 			int first = layer;
 			int last = n - 1 - layer;
 			
-			for (int i=first; i<last; ++i) {                                // updates elements within the layers
+			for (int i=first; i<last; ++i) {                              	     // updates elements within the layers
 				int offset = i - first;                                  
 				int top = matrix[first][i];                               
 				matrix[first][i] = matrix[last-offset][first];               // updates top edge with left edge    (top[i] =left[i])                 
 				matrix[last-offset][first] = matrix[last][last-offset];      // updates left edge with bottom edge (left[i] = bottom[i])
 				matrix[last][last-offset] = matrix[i][last];                 // updates bottom edge with right edge (bottom[i] = right[i])
-				matrix[i][last] = top;										 // updates right edge with top edge    (right[i] = top[i])
+				matrix[i][last] = top;					    // updates right edge with top edge    (right[i] = top[i])
 			}
 		}
 		System.out.println("After rotating matrix by 90 degree");            
-		for (int i=0; i<n; i++) {											// display result
+		for (int i=0; i<n; i++) {						    // display result
 			for (int j=0; j<n; j++) {
 				System.out.print(matrix[i][j]+"\t");
 			}
